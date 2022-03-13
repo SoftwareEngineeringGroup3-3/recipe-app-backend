ad5dbda85b81295c81ddbe86ee03291ab5006950
Noman <baba.noman@live.com>
2022-03-13 17:48:10 +0100

    Added repPassword parameter + Changed all registration-related error codes to 403

--- server/endpoints/registration.js
+++ server/endpoints/registration.js
@@ -10,16 +10,24 @@ const registrationValidation = {
 
 const checkDataUniqueness = (req, username, email) => {
     const sameUsers = req.database.prepare('SELECT user_id, user_name FROM users WHERE user_name = ?').all(username);
-    if(sameUsers.length > 0) throw new ApiError(409,'Database error: user with the same username already exists.'); //we assume user_name is unique
+    if(sameUsers.length > 0) throw new ApiError(409,'Parameters error: user with the same username already exists.'); //we assume user_name is unique
     const sameEmails = req.database.prepare('SELECT user_id, user_name FROM users WHERE user_email = ?').all(email);
-    if(sameEmails.length > 0) throw new ApiError(409,'Database error: user with the same email already exists.'); //we assume email is unique
+    if(sameEmails.length > 0) throw new ApiError(409,'Parameters error: user with the same email already exists.'); //we assume email is unique
     return true;
+}
+
+const checkPasswordMatch = (password, repPassword) => {
+    if(password==repPassword)
+        return true;
+    else
+        throw new ApiError(403,'Parameters error: password and repeated password don\'t match.');
 }
 
 class ApiRegistrationObject extends ApiObject {
     async post (req) {
         this.enforceContentType(req, 'application/json');
         const data = this.parseAndValidate(req.body, registrationValidation, true);
+        checkPasswordMatch(data.password,data.repPassword);
         checkDataUniqueness(req,data.username,data.email);
         var usrNew= new User(null,true);
         usrNew.username=data.username;
