28c8a1f939b61a7eecdb9e148a3c0749cdc41fe6
Noman <baba.noman@live.com>
2022-03-17 23:21:29 +0100

    FInished (and fixed, of course).

--- server/endpoints/ingredients.js
+++ server/endpoints/ingredients.js
@@ -7,21 +7,26 @@ const ingredientClassFormat = {
     name: { required: true, type: 'string', lambda: validateName },
     photo: { required: false, type: 'string'}
 };
-
+const checkDataUniqueness = (req, ingredientName) => {
+    const ingredientsWithSameName = req.database.prepare('SELECT ingredient_id FROM ingredients WHERE ingredient_name = ?').all(ingredientName);
+    if(ingredientsWithSameName.length > 0) throw new ApiError(403,'Parameters error: ingredient with the same name already exists.'); //we assume ingredient names are unique
+    return true;
+}
 class ApiIngredientObject extends ApiObject {
     async post (req) {
         this.enforceContentType(req, 'application/json');
-        const data = this.parseAndValidate(req.body, ingredientClassFormat, true);
+        //const data = this.parseAndValidate(req.body, ingredientClassFormat, false); //causes wrong error numbers to be returned. Replaced by simple parse in the following line:
+        const data= JSON.parse(req.body);
         if(!validateName(data.name))
         {
             throw new ApiError(403,'Validation exception: Incorrect or missing ingredient name.')
         }
+        checkDataUniqueness(req,data.name);
         var ingredient = new Ingredient();
-        ingredient.id=data.id; //Is this line needed? The API specificatio`n seems to suggest so, yet so it did for Registration but we removed this line from that.
+        //ingredient.id=data.id; //Is this line needed? The API specificatio`n seems to suggest so, yet so it did for Registration but we removed this line from that.
         ingredient.name=data.name;
         ingredient.photo=data.photo;
-        Console.log(ingredient.serialize());
-        ingredient.insert();
+        ingredient.insert(req.database);
         if(!ingredient.id)
         {
             throw new ApiError(403, 'Recipe creation failed');
