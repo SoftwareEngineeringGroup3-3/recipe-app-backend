644dff6b32ee94ce2b5598d53ce9fa423cb1c529
Noman <baba.noman@live.com>
2022-04-06 15:56:49 +0200

    Finished exaclty like we talked about.

--- server/endpoints/ingredients-next.js
+++ server/endpoints/ingredients-next.js
@@ -9,11 +9,6 @@ const ingredientNextFormat = {
 };
 
 
-const checkDataUniqueness = (req, ingredientName) => {
-    const ingredientsWithSameName = req.database.prepare('SELECT ingredient_id FROM ingredients WHERE ingredient_name = ?').all(ingredientName);
-    if(ingredientsWithSameName.length > 0) throw new ApiError(403,'Parameters error: ingredient with the same name already exists.'); //we assume ingredient names are unique
-    return true;
-}
 
 const countIngredients = (ingredientList) =>{
     let count = 0;
@@ -30,15 +25,14 @@ const countIngredients = (ingredientList) =>{
 const findMissingIngredients = (stringIngred, givenIngredients) =>{ //or ingredientLists if I want to do nested for loops.
     let ingredCount=countIngredients(stringIngred);
     let string='';
-    console.log(string=stringIngred.replace(/:.*;/,';'));
-    console.log(string=string.replace(/:.*/,'')); //handles the last ingredient because that doesn't always have ';'
+    string=stringIngred.replace(/:.*;/,';');
+    string=string.replace(/:.*/,''); //handles the last ingredient because that doesn't always have ';'
 
     const missingIngreds=[];
     let strIngredArr=string.split(';');
     for(let i=0; i<strIngredArr.length; i++){
         if(!givenIngredients.includes(Number(strIngredArr[i])))
         {
-            console.log(strIngredArr[i]);
             missingIngreds.push(strIngredArr[i]);
         }
     }
@@ -47,40 +41,48 @@ const findMissingIngredients = (stringIngred, givenIngredients) =>{ //or ingredi
 class ApiIngredientObject extends ApiObject {
     async get(req){
         console.log("endpoints/ingredients/next: recieved get");
-        // if(!req.user) {
-        //     throw new ApiError(401, 'User is not authorized!');
-        // }
+        if(!req.user) {
+            throw new ApiError(401, 'User is not authorized!');
+        }
 
-        ////Trying to do it the following way will become overwhelmingly complex if tried on current database structure
-        ////especially considering that some SQLite functions like LENGTH() aren't working.
-        const dataArray =[];
+        const givenIngredients =[];
         let givenIngredientsCount=0;
         const queryPart=[];
         for(const ingredient of JSON.parse(req.body))
         {
             this.validateFormat(ingredient,ingredientNextFormat,true)
-            console.log(typeof ingredient.id)
-            dataArray.push(ingredient.id);
+            givenIngredients.push(ingredient.id);
             queryPart.push(`recipe_ingredients LIKE \'%${ingredient.id}:%\'`);
             givenIngredientsCount++;
         }
-        const ingredientsListArray=[];
-        let recipesIngredientLists=req.database.prepare(`SELECT recipe_ingredients as RI FROM recipes WHERE ${queryPart.join(' AND ')} ORDER BY length(recipe_ingredients) DESC LIMIT 3 `).all(
-            // function(err,rows){
-            //     rows.forEach((row) => {
-            //         console.log(row.RI);
-            //     })
-            // }
-        );
-        console.log(recipesIngredientLists.length);
-        console.log(recipesIngredientLists[0]);
-        console.log(countIngredients(recipesIngredientLists[0]['RI']));
-        //let topRecipeIngredientCount=recipesWithPartialIngredients.//req.database.prepare('SELECT LEN(col) - LEN(REPLACE(col, 'Y', ''))')
-        //If we find a workaround for the above part and ingredient isn't found, return apierror 404 Ingredient not found.
-        
-        // var ingredients = req.database.prepare(`SELECT * FROM ingredients ORDER BY RANDOM() LIMIT 5;`).all();
-        // return ingredients;
-        return findMissingIngredients(recipesIngredientLists[0]['RI'],dataArray);
+
+        let recipesIngredientLists=req.database.prepare(
+            `SELECT recipe_ingredients as RI FROM recipes WHERE ${queryPart.join(' AND ')}
+            ORDER BY length(recipe_ingredients) DESC LIMIT 10 `)
+            .all();
+
+        let missingIngredients=[];
+        for(let i=0;i<recipesIngredientLists.length;i++)
+        {
+            missingIngredients=missingIngredients.concat(findMissingIngredients(recipesIngredientLists[i]['RI'],givenIngredients))
+        }
+
+        const ingredientsToReturn=[];
+        for(let i=0; i<5;i++)
+        {
+            let index= Math.floor(Math.random() * ((missingIngredients.length-1)+ 1));
+            if(!missingIngredients[index]=="" && !ingredientsToReturn.includes(missingIngredients[index])) //avoids duplicates
+            {
+                ingredientsToReturn.push(missingIngredients[index]);
+            }
+        }
+
+        if(ingredientsToReturn.length == 0)
+        {
+            throw new ApiError(404, "No suitable ingredients were found.");
+        }
+
+        return ingredientsToReturn;
     }
 
 }
