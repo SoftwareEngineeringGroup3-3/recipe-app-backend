0178523eece6eba7012b3ad42360e0c788ffeb09
Noman <baba.noman@live.com>
2022-05-11 22:12:16 +0200

    Perfected.
    The teacher said something like this is exactly what is needed towards the end of the classes so we don't need to think about that too much then.

--- server/endpoints/ingredients-next.js
+++ server/endpoints/ingredients-next.js
@@ -1,6 +1,9 @@
 const { ApiObject, ApiError } = require ('../apiobject.js');
 const { Ingredient, validateName, ingredientFormat } = require ('../templates/ingredients.js');
 const { User } = require('../templates/user.js');
+const Math = require('mathjs');
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
 const ingredientNextFormat = {
     id: {required: true, type: 'number', lambda: () => { return true; }},
@@ -45,47 +48,78 @@ class ApiIngredientObject extends ApiObject {
         }
 
         const givenIngredients =[];
+        const page = req.query.page??1;
+        const limit = req.query.limit??5;
         let givenIngredientsCount=0;
-        const queryPart=[];
-        for(const ingredient of JSON.parse(req.body))
+        let queryPart=[];
+
+        const parsedJson=JSON.parse(req.body)
+        try
         {
-            this.validateFormat(ingredient,ingredientNextFormat,true)
+            let i=0;
+            for(let templo of parsedJson)
+            {
+                break;
+            }
+        }
+        catch(err)
+        {
+            if (err.code==TypeError.code)
+                throw new ApiError(403,"Validation error: probably JSON is not iterable");
+        }
+        for(const ingredient of parsedJson) //maybe I should've used "in" instead of "of"??? Doesn't seem to trygger exception of not being iterable.
+        {
+            this.validateFormat(ingredient,ingredientNextFormat,true);
             givenIngredients.push(ingredient.id);
             queryPart.push(`recipe_ingredients LIKE \'%${ingredient.id}:%\'`);
             givenIngredientsCount++;
         }
-        if(queryPart.length==0)
+        if(queryPart.length<1)
         {
-            throw new ApiError(403, "No ingredients selected.")
+            //throw new ApiError(403, "No ingredients selected.")
+            queryPart=['1=1 '];
         }
 
         let recipesIngredientLists=req.database.prepare(
-            `SELECT recipe_ingredients as RI FROM recipes WHERE ${queryPart.join(' AND ')}
-            ORDER BY length(recipe_ingredients) DESC LIMIT 10 `)
-            .all();
-
+            `SELECT recipe_ingredients as RI FROM recipes 
+            WHERE ${queryPart.join(' AND ')} 
+            AND (length(recipe_ingredients) - 
+                length(replace(recipe_ingredients, ';', '')))>=${givenIngredientsCount}  `).all();
+            // ORDER BY random() DESC
+            // LIMIT ${limit} OFFSET ${(page-1)*limit} `) //was (page*limit) but now I'm assuming frontend will start sending from page=1
+            // .all();
+        
         let missingIngredients=[];
         for(let i=0;i<recipesIngredientLists.length;i++)
         {
-            missingIngredients=missingIngredients.concat(findMissingIngredients(recipesIngredientLists[i]['RI'],givenIngredients))
+            missingIngredients=missingIngredients.concat(findMissingIngredients(recipesIngredientLists[i]['RI'],givenIngredients));
         }
+        const total_ingredients = missingIngredients.length
+        const beginIndex= clamp((page-1)*limit,0,total_ingredients-1 + 99); //was -limit //+99 to let it be out of bounds so we can return empty array upon an invalid page limit combination
+        // const endIndex= clamp(page*limit,beginIndex, total_ingredients-1);
+        // if(beginIndex>=endIndex)
+        // {
+        //     endIndex= beginIndex+limit;
+        // }
+        const ingredients=missingIngredients.splice(beginIndex, limit)//endIndex-beginIndex); //assuming frontend sends page starting from 1 (not 0)
+        // const ingredientsToReturn=[];
+        // for(let i=0; i<5;i++)
+        // {
+        //     let index= Math.floor(Math.random() * ((missingIngredients.length-1)+ 1));
+        //     if(!missingIngredients[index]=="" && !ingredientsToReturn.includes(missingIngredients[index])) //avoids duplicates
+        //     {
+        //         ingredientsToReturn.push(missingIngredients[index]);
+        //     }
+        // }
 
-        const ingredientsToReturn=[];
-        for(let i=0; i<5;i++)
-        {
-            let index= Math.floor(Math.random() * ((missingIngredients.length-1)+ 1));
-            if(!missingIngredients[index]=="" && !ingredientsToReturn.includes(missingIngredients[index])) //avoids duplicates
-            {
-                ingredientsToReturn.push(missingIngredients[index]);
-            }
-        }
-
-        if(ingredientsToReturn.length == 0)
+        // if(ingredientsToReturn.length == 0)
+        if(ingredients.length==0)
         {
             throw new ApiError(404, "No suitable ingredients were found.");
         }
 
-        return ingredientsToReturn;
+        // return ingredientsToReturn;
+        return {total_ingredients, ingredients};
     }
 
 }
